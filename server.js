4dd3bd24073eb87ddebf38860202b59fbe4ae8da
Nate Boyette <nateboyette@icloud.com>
2019-01-24 17:17:19 -0800

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 let nextId = 7;
 
@@ -80,6 +80,20 @@ app.delete('/friends/:id', (req, res) => {
 	res.status(200).json(friends);
 });
 
-app.listen(5000, () => {
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('friendapp/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'friendapp', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log('server listening on port 5000');
 });
+
+
+
